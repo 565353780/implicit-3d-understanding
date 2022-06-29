31f903eebbef1dd97a56b9cc74424b20a58af92a
chLi <565353780@qq.com>
2022-06-29 18:45:19 +0800

    add common data collection

---
+++ Method/datasets.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Method.sun_rgbd.dataset import SUNRGBD, Total3D_Dataset
+from Method.pix3d.dataset import PIX3D
+from Method.mgnet.dataset import MGNet_Dataset
+from Method.ldif.dataset import PIX3DLDIF, LDIF_Dataset
+
