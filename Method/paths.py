5e839eb7ecf47c5b3f2f43feca755a2ebb784047
chLi <565353780@qq.com>
2022-06-30 15:05:57 +0800

    adjust config

--- Method/paths.py
+++ Method/paths.py
@@ -4,14 +4,14 @@
 import os
 
 def getModelPath(config):
-    model_dict = config['model']
-    if 'model_save_path' not in model_dict.keys():
+    log_dict = config['log']
+    if 'resume_path' not in log_dict.keys():
         return None
 
-    model_save_path = model_dict['model_save_path']
-    if model_save_path[-1] != "/":
-        model_save_path += "/"
-    last_model_path = model_save_path + "model_last.pth"
+    model_resume_path = log_dict['resume_path']
+    if model_resume_path[-1] != "/":
+        model_resume_path += "/"
+    last_model_path = model_resume_path + "model_last.pth"
     if not os.path.exists(last_model_path):
         return None
     return last_model_path
