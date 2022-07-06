34861d02f1cff24b7a31b30360dcb7c200901dd7
chLi <565353780@qq.com>
2022-07-06 20:47:08 +0800

    move pix3d-config into Config folder

--- Method/ldif/dataset.py
+++ Method/ldif/dataset.py
@@ -14,17 +14,7 @@ from tqdm import tqdm
 
 from external.ldif.util import gaps_util, file_util
 
-class PIX3DConfig(object):
-    def __init__(self):
-        self.root_path = './data/pix3d/'
-        self.train_split = self.root_path + 'splits/train.json'
-        self.test_split = self.root_path + 'splits/test.json'
-        self.metadata_path = self.root_path + 'metadata'
-        self.metadata_file = self.metadata_path + '/pix3d.json'
-        self.classnames = ['misc',
-                           'bed', 'bookcase', 'chair', 'desk', 'sofa',
-                           'table', 'tool', 'wardrobe']
-        return
+from Config.pix3d import PIX3DConfig
 
 class PIX3DLDIF(Dataset):
     def __init__(self, config, mode):
