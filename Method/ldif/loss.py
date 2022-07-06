575fbb7aa59431a1da4e528cd70d96513722638a
chLi <565353780@qq.com>
2022-07-06 20:10:20 +0800

    refine code format

--- Method/ldif/loss.py
+++ Method/ldif/loss.py
@@ -12,7 +12,6 @@ class LDIFLoss(object):
         return
 
     def __call__(self, est_data, gt_data):
-        # calculate loss (ldif.training.loss.compute_loss)
         uniform_sample_loss = nn.MSELoss()(est_data['uniform_class'], gt_data['uniform_class'])
         uniform_sample_loss *= self.config['model']['uniform_loss_weight']
 
@@ -20,18 +19,28 @@ class LDIFLoss(object):
         near_surface_sample_loss *= self.config['model']['near_surface_loss_weight']
 
         element_centers = est_data['element_centers']
-        xyzw_samples = F.pad(element_centers, [0, 1], "constant", 1) # 维度为 [batch_size, sample_count, 4]
-        xyzw_samples = torch.matmul(xyzw_samples, gt_data['world2grid'])[..., :3] # 维度为 [batch_size, sample_count, 3]
-        # xyzw_samples = torch.matmul(torch.Tensor([[[1.2] * 3 + [1]], ] * xyzw_samples.shape[0]).to(element_centers.device), gt_data['world2grid'])[..., :3] # 测试边界
+
+        #  dim = [batch_size, sample_count, 4]
+        xyzw_samples = F.pad(element_centers, [0, 1], "constant", 1)
+
+        # dim = [batch_size, sample_count, 3]
+        xyzw_samples = torch.matmul(xyzw_samples, gt_data['world2grid'])[..., :3]
+
         grid = gt_data['grid']
         scale_fac = torch.Tensor(list(grid.shape)[1:]).to(element_centers.device) / 2 - 0.5
         xyzw_samples /= scale_fac
-        xyzw_samples = xyzw_samples.unsqueeze(1).unsqueeze(1) # 维度为 [batch_size, 1, 1, sample_count, 3]
+
+        # dim = [batch_size, 1, 1, sample_count, 3]
+        xyzw_samples = xyzw_samples.unsqueeze(1).unsqueeze(1)
+
         grid = grid.unsqueeze(1)
-        gt_sdf_at_centers = F.grid_sample(grid, xyzw_samples, mode='bilinear', padding_mode='zeros') # https://pytorch.org/docs/master/nn.functional.html#torch.nn.functional.grid_sample
+
+        gt_sdf_at_centers = F.grid_sample(grid, xyzw_samples, mode='bilinear', padding_mode='zeros')
+
         gt_sdf_at_centers = torch.where(gt_sdf_at_centers > self.config['data']['coarse_grid_spacing'] / 1.1,
                                         gt_sdf_at_centers, torch.zeros(1).to(gt_sdf_at_centers.device))
         gt_sdf_at_centers *= self.config['model']['lowres_grid_inside_loss_weight']
+
         element_center_lowres_grid_inside_loss = torch.mean((gt_sdf_at_centers + 1e-04) ** 2) + 1e-05
 
         bounding_box = self.config['data']['bounding_box']
