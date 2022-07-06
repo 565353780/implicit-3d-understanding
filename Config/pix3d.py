2fa9baf415abd607250046a279bbb890c406744c
chLi <565353780@qq.com>
2022-07-06 21:48:35 +0800

    update param

--- Config/pix3d.py
+++ Config/pix3d.py
@@ -6,8 +6,8 @@ class PIX3DConfig(object):
         self.root_path = './data/pix3d/'
         self.train_split = self.root_path + 'splits/train.json'
         self.test_split = self.root_path + 'splits/test.json'
-        self.metadata_path = self.root_path + 'metadata'
-        self.metadata_file = self.metadata_path + '/pix3d.json'
+        self.metadata_path = self.root_path + 'metadata/'
+        self.metadata_file = self.metadata_path + 'pix3d.json'
         self.classnames = ['misc',
                            'bed', 'bookcase', 'chair', 'desk', 'sofa',
                            'table', 'tool', 'wardrobe']
