0397611c2acee6216987f65a0085d0b3ff4f0870
chLi <565353780@qq.com>
2022-11-16 15:41:43 +0800

    move build bash commands into setup bash file

--- setup.sh
+++ setup.sh
@@ -3,3 +3,31 @@ sudo apt install mesa-common-dev libglu1-mesa-dev libosmesa6-dev libxi-dev libgl
 sudo apt install --reinstall libgl1-mesa-glx -y
 sudo apt-get install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
 
+pip install torch torchvision torchaudio \
+      --extra-index-url https://download.pytorch.org/whl/cu113
+pip install wandb tqdm cython pytz python-dateutil \
+      trimesh scipy scikit-image shapely jellyfish \
+      vtk seaborn h5py opencv-python
+
+HOROVOD_GPU_OPERATIONS=NCCL pip install --no-cache-dir horovod
+
+cd external/ldif/gaps
+make mesa -j
+
+cd ../../mesh_fusion/libfusiongpu
+mkdir build
+cd build
+cmake ..
+make -j
+cd ..
+python setup.py build_ext -i -f
+cd ../librender
+python setup.py build_ext -i -f
+cd ../libmcubes
+python setup.py build_ext -i -f
+
+cd ../../ldif/ldif2mesh
+bash build.sh
+
+cd ../../..
+
