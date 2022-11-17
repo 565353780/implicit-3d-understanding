31e02035c1c0ec62a3856d1293bbc53b682c8954
chLi <565353780@qq.com>
2022-11-17 21:52:05 +0800

    remove unused switch code

--- pose_detect/Model/total3d.py
+++ pose_detect/Model/total3d.py
@@ -96,120 +96,90 @@ class TOTAL3D(BaseNetwork):
     def forward(self, data):
         all_output = {}
 
-        if self.cfg.config[self.cfg.config['mode']]['phase'] in [
-                'layout_estimation', 'joint'
-        ]:
-            pitch_reg_result, roll_reg_result, \
-            pitch_cls_result, roll_cls_result, \
-            lo_ori_reg_result, lo_ori_cls_result, \
-            lo_centroid_result, lo_coeffs_result, a_features = self.layout_estimation(data['image'])
+        pitch_reg_result, roll_reg_result, \
+        pitch_cls_result, roll_cls_result, \
+        lo_ori_reg_result, lo_ori_cls_result, \
+        lo_centroid_result, lo_coeffs_result, a_features = self.layout_estimation(data['image'])
 
-            layout_output = {
-                'pitch_reg_result': pitch_reg_result,
-                'roll_reg_result': roll_reg_result,
-                'pitch_cls_result': pitch_cls_result,
-                'roll_cls_result': roll_cls_result,
-                'lo_ori_reg_result': lo_ori_reg_result,
-                'lo_ori_cls_result': lo_ori_cls_result,
-                'lo_centroid_result': lo_centroid_result,
-                'lo_coeffs_result': lo_coeffs_result,
-                'lo_afeatures': a_features
-            }
-            all_output.update(layout_output)
+        layout_output = {
+            'pitch_reg_result': pitch_reg_result,
+            'roll_reg_result': roll_reg_result,
+            'pitch_cls_result': pitch_cls_result,
+            'roll_cls_result': roll_cls_result,
+            'lo_ori_reg_result': lo_ori_reg_result,
+            'lo_ori_cls_result': lo_ori_cls_result,
+            'lo_centroid_result': lo_centroid_result,
+            'lo_coeffs_result': lo_coeffs_result,
+            'lo_afeatures': a_features
+        }
+        all_output.update(layout_output)
 
-        if self.cfg.config[self.cfg.config['mode']]['phase'] in [
-                'object_detection', 'joint'
-        ]:
-            size_reg_result, \
-            ori_reg_result, ori_cls_result, \
-            centroid_reg_result, centroid_cls_result, \
-            offset_2D_result, a_features, \
-            r_features, a_r_features = self.object_detection(data['patch'], data['size_cls'], data['g_features'],
-                                                             data['split'], data['rel_pair_counts'])
-            object_output = {
-                'size_reg_result': size_reg_result,
-                'ori_reg_result': ori_reg_result,
-                'ori_cls_result': ori_cls_result,
-                'centroid_reg_result': centroid_reg_result,
-                'centroid_cls_result': centroid_cls_result,
-                'offset_2D_result': offset_2D_result,
-                'odn_afeature': a_features,
-                'odn_rfeatures': r_features,
-                'odn_arfeatures': a_r_features
-            }
-            all_output.update(object_output)
+        size_reg_result, \
+        ori_reg_result, ori_cls_result, \
+        centroid_reg_result, centroid_cls_result, \
+        offset_2D_result, a_features, \
+        r_features, a_r_features = self.object_detection(data['patch'], data['size_cls'], data['g_features'],
+                                                         data['split'], data['rel_pair_counts'])
+        object_output = {
+            'size_reg_result': size_reg_result,
+            'ori_reg_result': ori_reg_result,
+            'ori_cls_result': ori_cls_result,
+            'centroid_reg_result': centroid_reg_result,
+            'centroid_cls_result': centroid_cls_result,
+            'offset_2D_result': offset_2D_result,
+            'odn_afeature': a_features,
+            'odn_rfeatures': r_features,
+            'odn_arfeatures': a_r_features
+        }
+        all_output.update(object_output)
 
-        if self.cfg.config[self.cfg.config['mode']]['phase'] in ['joint']:
-            # predict meshes
-            if self.cfg.config['mode'] == 'train':
-                if isinstance(self.mesh_reconstruction.module,
-                              MODULES.get('LDIF')):
-                    # for LDIF
-                    mesh_output = self.mesh_reconstruction(
-                        data['patch_for_mesh'],
-                        data['cls_codes'],
-                        structured_implicit=True)
-                else:
-                    # for MGNet
-                    mesh_output = self.mesh_reconstruction(
-                        data['patch_for_mesh'],
-                        data['cls_codes'],
-                        mask_status=data['mask_status'])
-                    if data['mask_flag'] == 1:
-                        meshes = mesh_output['mesh_coordinates_results'][-1]
-                        mesh_output['meshes'] = meshes
-                    else:
-                        mesh_output['meshes'] = None
-            else:
-                # for test
-                reconstruction = 'mesh' if self.cfg.config['full'] else None
-                mesh_output = self.mesh_reconstruction(
-                    data['patch_for_mesh'],
-                    data['cls_codes'],
-                    reconstruction=reconstruction)
-                out_points = mesh_output.get('mesh_coordinates_results',
-                                             [None])
-                out_faces = mesh_output.get('faces', None)
-                mesh_output.update({
-                    'meshes': out_points[-1],
-                    'out_faces': out_faces
-                })
-
-            # construct structured_implicti from dict
-            if 'structured_implicit' in mesh_output:
-                mesh_output['structured_implicit'] = StructuredImplicit(
-                    config=self.cfg.config,
-                    **mesh_output['structured_implicit'])
-
-            # convert to SUNRGBD coordinates
-            if mesh_output.get('meshes') is not None:
-                if isinstance(mesh_output['meshes'], list):
-                    for m in mesh_output['meshes']:
-                        m[2, :] *= -1
-                elif mesh_output['meshes'] is not None:
-                    mesh_output['meshes'][:, 2, :] *= -1
-            mesh_output['mgn'] = self.mesh_reconstruction
-            all_output.update(mesh_output)
-
-            # get extra_results
-            all_output.update(self.get_extra_results(all_output))
-
-            if hasattr(self, 'output_adjust'):
-                input = all_output.copy()
-                input['size_cls'] = data['size_cls']
-                input['cls_codes'] = data['cls_codes']
-                input['g_features'] = data['g_features']
-                input['bdb2D_pos'] = data['bdb2D_pos']
-                input['K'] = data['K']
-                input['split'] = data['split']
-                input['rel_pair_counts'] = data['rel_pair_counts']
-                refined_output = self.output_adjust(input)
-                all_output.update(refined_output)
-
-        if all_output:
-            return all_output
+        # predict meshes
+        if self.cfg.config['mode'] == 'train':
+            mesh_output = self.mesh_reconstruction(data['patch_for_mesh'],
+                                                   data['cls_codes'],
+                                                   structured_implicit=True)
         else:
-            raise NotImplementedError
+            mesh_output = self.mesh_reconstruction(data['patch_for_mesh'],
+                                                   data['cls_codes'],
+                                                   reconstruction='mesh')
+            out_points = mesh_output.get('mesh_coordinates_results', [None])
+            out_faces = mesh_output.get('faces', None)
+            mesh_output.update({
+                'meshes': out_points[-1],
+                'out_faces': out_faces
+            })
+
+        # construct structured_implicti from dict
+        if 'structured_implicit' in mesh_output:
+            mesh_output['structured_implicit'] = StructuredImplicit(
+                config=self.cfg.config, **mesh_output['structured_implicit'])
+
+        # convert to SUNRGBD coordinates
+        if mesh_output.get('meshes') is not None:
+            if isinstance(mesh_output['meshes'], list):
+                for m in mesh_output['meshes']:
+                    m[2, :] *= -1
+            elif mesh_output['meshes'] is not None:
+                mesh_output['meshes'][:, 2, :] *= -1
+        mesh_output['mgn'] = self.mesh_reconstruction
+        all_output.update(mesh_output)
+
+        # get extra_results
+        all_output.update(self.get_extra_results(all_output))
+
+        if hasattr(self, 'output_adjust'):
+            input = all_output.copy()
+            input['size_cls'] = data['size_cls']
+            input['cls_codes'] = data['cls_codes']
+            input['g_features'] = data['g_features']
+            input['bdb2D_pos'] = data['bdb2D_pos']
+            input['K'] = data['K']
+            input['split'] = data['split']
+            input['rel_pair_counts'] = data['rel_pair_counts']
+            refined_output = self.output_adjust(input)
+            all_output.update(refined_output)
+
+        return all_output
 
     def loss(self, est_data, gt_data):
         '''
