ae23e215e89069ce9a5ec766d8a4f170032f4645
chLi <565353780@qq.com>
2022-11-27 20:09:26 +0800

    set some params directly in LDIFReconLoss, remove config as input

--- pose_detect/Loss/ldif_loss.py
+++ pose_detect/Loss/ldif_loss.py
@@ -94,101 +94,87 @@ class LDIFLoss(object):
 
 class LDIFReconLoss(object):
 
-    def __init__(self, config=None, weight=1):
-        self.config = config
+    def __init__(self, weight=1):
         self.weight = weight
         return
 
     def __call__(self, est_data, gt_data, extra_results):
         ldif = est_data['mgn']
-        get_phy_loss = self.config.get('loss_weights', {}).get(
-            'ldif_phy_loss', 0.0) > 0
         device = gt_data['image'].device
 
-        loss_settings = self.config['model']['mesh_reconstruction'].get(
-            'loss_settings', {})
-        loss_type = loss_settings.get('type', 'classmse')
-        scale_before_func = loss_settings.get('scale_before_func', 100)
-        phy_loss_samples = loss_settings.get('phy_loss_samples', 128)
-        phy_loss_objects = loss_settings.get('phy_loss_objects', 128)
-        surface_optimize = self.config['model']['mesh_reconstruction'][
-            'loss_settings'].get('surface_optimize', False)
+        loss_type = 'classmse'
+        scale_before_func = 100.0
+        phy_loss_samples = 128
+        phy_loss_objects = 4
+        surface_optimize = True
         sdf_data = {}
 
-        if get_phy_loss:
-            bdb3D_form = get_bdb_form_from_corners(
-                extra_results['bdb3D_result'])
-            structured_implicit = est_data['structured_implicit']
-            ldif_center, ldif_coef = est_data['obj_center'], est_data[
-                'obj_coef']
-            if 'ldif_sampling_points' in est_data:
-                inside_samples = est_data['ldif_sampling_points']
-            else:
-                obj_center = ldif_center.clone()
-                obj_center[:, 2] *= -1
-                inside_samples = get_phy_loss_samples(
-                    ldif,
-                    structured_implicit,
-                    obj_center,
-                    ldif_coef,
-                    phy_loss_samples,
-                    surface_optimize=surface_optimize)
+        bdb3D_form = get_bdb_form_from_corners(extra_results['bdb3D_result'])
+        structured_implicit = est_data['structured_implicit']
+        ldif_center, ldif_coef = est_data['obj_center'], est_data['obj_coef']
+        if 'ldif_sampling_points' in est_data:
+            inside_samples = est_data['ldif_sampling_points']
+        else:
+            obj_center = ldif_center.clone()
+            obj_center[:, 2] *= -1
+            inside_samples = get_phy_loss_samples(
+                ldif,
+                structured_implicit,
+                obj_center,
+                ldif_coef,
+                phy_loss_samples,
+                surface_optimize=surface_optimize)
 
-            # put points to other objects' coor
-            inside_samples[:, :, 2] *= -1
-            obj_samples = recover_points_to_world_sys(bdb3D_form,
-                                                      inside_samples,
-                                                      ldif_center, ldif_coef)
-            max_sample_points = (gt_data['split'][:, 1] -
-                                 gt_data['split'][:, 0] -
-                                 1).max() * obj_samples.shape[1]
-            if max_sample_points == 0:
+        # put points to other objects' coor
+        inside_samples[:, :, 2] *= -1
+        obj_samples = recover_points_to_world_sys(bdb3D_form, inside_samples,
+                                                  ldif_center, ldif_coef)
+        max_sample_points = (gt_data['split'][:, 1] - gt_data['split'][:, 0] -
+                             1).max() * obj_samples.shape[1]
+        if max_sample_points == 0:
+            sdf_data['ldif_phy_loss'] = None
+        else:
+            est_sdf = []
+            for start, end in gt_data['split']:
+                assert end > start
+                if end > start + 1:
+                    centroids = bdb3D_form['centroid'][start:end]
+                    centroids = centroids.unsqueeze(0).expand(
+                        len(centroids), -1, -1)
+                    distances = F.pairwise_distance(
+                        centroids.reshape(-1, 3),
+                        centroids.transpose(0, 1).reshape(-1, 3),
+                        2).reshape(len(centroids), len(centroids))
+                    for obj_ind in range(start, end):
+                        other_obj_dis = distances[obj_ind - start]
+                        _, nearest = torch.sort(other_obj_dis)
+                        other_obj_sample = obj_samples[start:end].index_select(
+                            0, nearest[1:phy_loss_objects + 1]).reshape(-1, 3)
+                        other_obj_sample = recover_points_to_obj_sys(
+                            {
+                                k: v[obj_ind:obj_ind + 1]
+                                for k, v in bdb3D_form.items()
+                            }, other_obj_sample.unsqueeze(0),
+                            ldif_center[obj_ind:obj_ind + 1],
+                            ldif_coef[obj_ind:obj_ind + 1])
+                        other_obj_sample[:, :, 2] *= -1
+                        sdf = ldif(
+                            samples=other_obj_sample,
+                            structured_implicit=structured_implicit[
+                                obj_ind:obj_ind + 1].dict(),
+                            apply_class_transfer=False,
+                        )['global_decisions']
+                        est_sdf.append(sdf.squeeze())
+            if len(est_sdf) == 0:
                 sdf_data['ldif_phy_loss'] = None
             else:
-                est_sdf = []
-                for start, end in gt_data['split']:
-                    assert end > start
-                    if end > start + 1:
-                        centroids = bdb3D_form['centroid'][start:end]
-                        centroids = centroids.unsqueeze(0).expand(
-                            len(centroids), -1, -1)
-                        distances = F.pairwise_distance(
-                            centroids.reshape(-1, 3),
-                            centroids.transpose(0, 1).reshape(-1, 3),
-                            2).reshape(len(centroids), len(centroids))
-                        for obj_ind in range(start, end):
-                            other_obj_dis = distances[obj_ind - start]
-                            _, nearest = torch.sort(other_obj_dis)
-                            other_obj_sample = obj_samples[
-                                start:end].index_select(
-                                    0,
-                                    nearest[1:phy_loss_objects + 1]).reshape(
-                                        -1, 3)
-                            other_obj_sample = recover_points_to_obj_sys(
-                                {
-                                    k: v[obj_ind:obj_ind + 1]
-                                    for k, v in bdb3D_form.items()
-                                }, other_obj_sample.unsqueeze(0),
-                                ldif_center[obj_ind:obj_ind + 1],
-                                ldif_coef[obj_ind:obj_ind + 1])
-                            other_obj_sample[:, :, 2] *= -1
-                            sdf = ldif(
-                                samples=other_obj_sample,
-                                structured_implicit=structured_implicit[
-                                    obj_ind:obj_ind + 1].dict(),
-                                apply_class_transfer=False,
-                            )['global_decisions']
-                            est_sdf.append(sdf.squeeze())
-                if len(est_sdf) == 0:
-                    sdf_data['ldif_phy_loss'] = None
-                else:
-                    est_sdf = torch.cat(est_sdf) + 0.07
-                    est_sdf[est_sdf > 0] = 0
-                    gt_sdf = torch.full(est_sdf.shape,
-                                        0.,
-                                        device=device,
-                                        dtype=torch.float32)
-                    sdf_data['ldif_phy_loss'] = (est_sdf, gt_sdf)
+                est_sdf = torch.cat(est_sdf) + 0.07
+                est_sdf[est_sdf > 0] = 0
+                gt_sdf = torch.full(est_sdf.shape,
+                                    0.,
+                                    device=device,
+                                    dtype=torch.float32)
+                sdf_data['ldif_phy_loss'] = (est_sdf, gt_sdf)
 
         # compute final loss
         loss = {}
