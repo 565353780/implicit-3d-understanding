6253cf9e2ff0a8f3fa1ff10e08466d2f8f942284
chLi <565353780@qq.com>
2022-11-27 22:53:13 +0800

    split gcnn into several unit modules

---
+++ pose_detect/Model/gcnn/gclayer_collect.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from torch import nn
+
+from pose_detect.Model.gcnn.collection_unit import CollectionUnit
+
+
+class GraphConvolutionLayerCollect(nn.Module):
+    """ graph convolutional layer """
+    """ collect information from neighbors """
+
+    def __init__(self, dim_obj, dim_rel):
+        super().__init__()
+        self.collect_units = nn.ModuleList()
+        self.collect_units.append(CollectionUnit(
+            dim_rel, dim_obj))  # obj (subject) from rel
+        self.collect_units.append(CollectionUnit(
+            dim_rel, dim_obj))  # obj (object) from rel
+        self.collect_units.append(CollectionUnit(
+            dim_obj, dim_rel))  # rel from obj (subject)
+        self.collect_units.append(CollectionUnit(
+            dim_obj, dim_rel))  # rel from obj (object)
+        self.collect_units.append(CollectionUnit(dim_obj,
+                                                 dim_obj))  # obj from obj
+
+    def forward(self, target, source, attention, unit_id):
+        collection = self.collect_units[unit_id](target, source, attention)
+        return collection
--- pose_detect/Model/gcnn/gcnn.py
+++ pose_detect/Model/gcnn/gcnn.py
@@ -3,89 +3,15 @@
 
 import torch
 import torch.nn as nn
-from models.registers import MODULES
 from configs.data_config import NYU40CLASSES, pix3d_n_classes
 import torch.nn.functional as F
 from net_utils.libs import get_bdb_form_from_corners, recover_points_to_world_sys, \
     get_rotation_matix_result, get_bdb_3d_result, recover_points_to_obj_sys
 
-
-def normal_init(m, mean, stddev, truncated=False):
-    if truncated:
-        m.weight.data.normal_().fmod_(2).mul_(stddev).add_(
-            mean)  # not a perfect approximation
-    else:
-        m.weight.data.normal_(mean, stddev)
-        m.bias.data.zero_()
+from pose_detect.Model.gcnn.gclayer_collect import GraphConvolutionLayerCollect
+from pose_detect.Model.gcnn.gclayer_update import GraphConvolutionLayerUpdate
 
 
-class _Collection_Unit(nn.Module):
-
-    def __init__(self, dim_in, dim_out):
-        super(_Collection_Unit, self).__init__()
-        self.fc = nn.Linear(dim_in, dim_out, bias=True)
-        normal_init(self.fc, 0, 0.01)
-
-    def forward(self, target, source, attention_base):
-        # assert attention_base.size(0) == source.size(0), "source number must be equal to attention number"
-        fc_out = F.relu(self.fc(source))
-        collect = torch.mm(attention_base, fc_out)  # Nobj x Nrel Nrel x dim
-        collect_avg = collect / (
-            attention_base.sum(1).view(collect.size(0), 1) + 1e-7)
-        return collect_avg
-
-
-class _Update_Unit(nn.Module):
-
-    def __init__(self, dim):
-        super(_Update_Unit, self).__init__()
-
-    def forward(self, target, source):
-        assert target.size() == source.size(
-        ), "source dimension must be equal to target dimension"
-        update = target + source
-        return update
-
-
-class _GraphConvolutionLayer_Collect(nn.Module):
-    """ graph convolutional layer """
-    """ collect information from neighbors """
-
-    def __init__(self, dim_obj, dim_rel):
-        super(_GraphConvolutionLayer_Collect, self).__init__()
-        self.collect_units = nn.ModuleList()
-        self.collect_units.append(_Collection_Unit(
-            dim_rel, dim_obj))  # obj (subject) from rel
-        self.collect_units.append(_Collection_Unit(
-            dim_rel, dim_obj))  # obj (object) from rel
-        self.collect_units.append(_Collection_Unit(
-            dim_obj, dim_rel))  # rel from obj (subject)
-        self.collect_units.append(_Collection_Unit(
-            dim_obj, dim_rel))  # rel from obj (object)
-        self.collect_units.append(_Collection_Unit(dim_obj,
-                                                   dim_obj))  # obj from obj
-
-    def forward(self, target, source, attention, unit_id):
-        collection = self.collect_units[unit_id](target, source, attention)
-        return collection
-
-
-class _GraphConvolutionLayer_Update(nn.Module):
-    """ graph convolutional layer """
-    """ update target nodes """
-
-    def __init__(self, dim_obj, dim_rel):
-        super(_GraphConvolutionLayer_Update, self).__init__()
-        self.update_units = nn.ModuleList()
-        self.update_units.append(_Update_Unit(dim_obj))  # obj from others
-        self.update_units.append(_Update_Unit(dim_rel))  # rel from others
-
-    def forward(self, target, source, unit_id):
-        update = self.update_units[unit_id](target, source)
-        return update
-
-
-@MODULES.register_module
 class GCNN(nn.Module):
 
     def __init__(self, cfg, optim_spec=None):
@@ -176,11 +102,11 @@ class GCNN(nn.Module):
         # graph message passing (from graph-rcnn)
         if self.feat_update_step > 0:
             self.gcn_collect_feat = nn.ModuleList([
-                _GraphConvolutionLayer_Collect(feature_dim, feature_dim)
+                GraphConvolutionLayerCollect(feature_dim, feature_dim)
                 for i in range(self.feat_update_group)
             ])
             self.gcn_update_feat = nn.ModuleList([
-                _GraphConvolutionLayer_Update(feature_dim, feature_dim)
+                GraphConvolutionLayerUpdate(feature_dim, feature_dim)
                 for i in range(self.feat_update_group)
             ])
 
