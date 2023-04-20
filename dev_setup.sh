10abf56fa345940cb553222834e08b13cbd5b424
chLi <565353780@qq.com>
2023-04-20 22:29:33 +0800

    update cuda version

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,8 +3,9 @@ sudo apt install mesa-common-dev libglu1-mesa-dev libosmesa6-dev libxi-dev libgl
 sudo apt install --reinstall libgl1-mesa-glx -y
 sudo apt-get install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
 
-pip install torch torchvision torchaudio \
-      --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 \
+  --extra-index-url https://download.pytorch.org/whl/cu116
+
 pip install wandb tqdm cython pytz python-dateutil \
       trimesh scipy scikit-image shapely jellyfish \
       vtk seaborn h5py opencv-python tensorboard
