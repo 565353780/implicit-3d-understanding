da98f61609df68c5593ca743d3950aaf7d0abbf3
chLi <565353780@qq.com>
2023-11-04 19:17:20 +0800

    set apt install as auto accept

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 sudo apt install xvfb ninja-build freeglut3-dev libglew-dev meshlab -y
 sudo apt install mesa-common-dev libglu1-mesa-dev libosmesa6-dev libxi-dev libgl1-mesa-dev -y
 sudo apt install --reinstall libgl1-mesa-glx -y
-sudo apt-get install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
+sudo apt-get install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev -y
 
 pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 \
-  --extra-index-url https://download.pytorch.org/whl/cu116
+	--extra-index-url https://download.pytorch.org/whl/cu116
 
 pip install wandb tqdm cython pytz python-dateutil \
-      trimesh scipy scikit-image shapely jellyfish \
-      vtk seaborn h5py opencv-python tensorboard
+	trimesh scipy scikit-image shapely jellyfish \
+	vtk seaborn h5py opencv-python tensorboard
 
 cd external/ldif/gaps
 make mesa -j
@@ -29,4 +29,3 @@ cd ../../ldif/ldif2mesh
 bash build.sh
 
 cd ../../..
-
